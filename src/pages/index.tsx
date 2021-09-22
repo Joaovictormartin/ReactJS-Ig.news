bc007fdb7801d57857791575cd098ab84e52b250
Joao <joao.teixeira@aedb.br>
2021-09-22 13:38:08 -0300

    configurando a api de pagemento

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,37 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { stripe } from "../services/stripe";
 
 import { SubscribeButton } from "../components/SubscribeButton";
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>✌ Hey, welcome!</span>
-          <h1>News about the <span>React</span> world</h1>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month
+            </span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/svg/avatar.svg" alt="Girl coding" />
@@ -27,3 +39,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JcDLMHun3qN76GltPpqiLof");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
