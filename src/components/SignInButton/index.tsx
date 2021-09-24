dfc623a1e02a2929769139eee0f7dcf4eb30a89e
Joao <joao.teixeira@aedb.br>
2021-09-24 14:40:58 -0300

    configurando o button do github para receber o SingIn e o SingOut do GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
-import { FaGithub } from "react-icons/fa";
-import { FiX } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa"; //Icon Github
+import { FiX } from "react-icons/fi"; // Icon Fechar
+
+import { signIn, signOut, useSession,  } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.SignInButton}>
+  return session ? (
+    <button 
+      type="button" 
+      className={styles.SignInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      João Victor
-      <FiX color="#737380" className={styles.closeIcon}/>
+      {session.user?.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
-
   ) : (
-
-    <button type="button" className={styles.SignInButton}>
+    <button
+      type="button"
+      className={styles.SignInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sing in with GitHub
     </button>
